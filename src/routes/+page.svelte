355f16956365a8233bc2f549218ec3d5db92e8d8
craigfay <craigfay1@gmail.com>
2022-09-07 14:43:23 -0600

    Reset preview styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,36 @@
 <script>
-  let mode = 'edit';
+  import Button from '$lib/Button.svelte';
+  import showdown  from 'showdown';
+  const converter = new showdown.Converter();
 
-  let editorContent = ''
+  let mode = 'preview';
+  let editorContent = '## Hello!\n\n_How are you?_';
+
+  $: htmlContent = converter.makeHtml(editorContent);
+  $: buttonText = mode == 'edit' ? 'Preview' : 'Edit';
+
+  const toggleMode = () => mode = mode == 'edit' ? 'preview' : 'edit';
+
 </script>
 
 <div class="m-4 flex flex-col max-w-full border-box">
+  <Button onClick={toggleMode} class="mb-4">{buttonText}</Button>
 
   {#if mode == 'edit'}
-    <div class="p-2 font-mono border border-box border-gray-200" contenteditable bind:innerHTML={editorContent}></div>
+    <div data-editor class="p-2 font-mono border border-box border-gray-200" contenteditable bind:innerHTML={editorContent}></div>
   {/if}
 
   {#if mode == 'preview'}
-    <div>
-
-    </div>
+    <div data-preview="">{@html htmlContent}</div>
   {/if}
 
 </div>
+
+<style>
+  :global([data-preview=''] *) {
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+    font-weight: revert;
+    font-size: revert;
+  }
+
+</style>
