a4a23e09f143267640e31eaa708a52e1f16451d3
craigfay <craigfay1@gmail.com>
2022-09-08 23:13:56 -0500

    Resizeable editor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,53 @@
 <script>
   import Button from '$lib/Button.svelte';
-  import showdown  from 'showdown';
+  import showdown from 'showdown';
+
   const converter = new showdown.Converter();
 
-  let mode = 'preview';
-  let editorContent = '## Hello!\n\n_How are you?_';
+  let editor;
+  let mode = 'edit';
 
-  $: htmlContent = converter.makeHtml(editorContent);
+  export let editorContent = '## Title\n\n_Enter some text here_';
+
   $: buttonText = mode == 'edit' ? 'Preview' : 'Edit';
+  $: previewContent = converter.makeHtml(editorContent);
 
-  const toggleMode = () => mode = mode == 'edit' ? 'preview' : 'edit';
+  const toggleMode = () => {
+    mode = mode == 'edit' ? 'preview' : 'edit';
+  };
+
+  function resizeEditor() {
+    editor.style.height = editor.scrollHeight + 'px';
+  }
 
 </script>
+
 
 <div class="m-4 flex flex-col max-w-full border-box">
   <Button onClick={toggleMode} class="mb-4">{buttonText}</Button>
 
   {#if mode == 'edit'}
-    <div data-editor class="p-2 font-mono border border-box border-gray-200" contenteditable bind:innerHTML={editorContent}></div>
+    <textarea
+      data-editor
+      bind:this={editor}
+      class="p-2 font-mono resize-none focus:outline-none"
+      style={`height:${editor?.scrollHeight}px`}
+      on:input={resizeEditor}
+      bind:value={editorContent}
+    />
   {/if}
 
   {#if mode == 'preview'}
-    <div data-preview="">{@html htmlContent}</div>
+    <div data-preview="">{@html previewContent}</div>
   {/if}
 
 </div>
 
 <style>
   :global([data-preview=''] *) {
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji';
     font-weight: revert;
     font-size: revert;
+    margin-bottom: 1rem;
   }
-
 </style>
