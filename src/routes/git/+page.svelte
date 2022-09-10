c31bedf12c60b2eb41301df809ff5427ccca4042
craigfay <craigfay1@gmail.com>
2022-09-10 00:46:50 -0500

    Introduce isomorphic git POC

---
+++ src/routes/git/+page.svelte
@@ -0,0 +1,30 @@
+
+<script>
+  export const ssr = false;
+
+  import { onMount } from 'svelte';
+  import git from 'isomorphic-git'
+  import http from 'isomorphic-git/http/web'
+  import FileSystem from '@isomorphic-git/lightning-fs'
+  import Button from '$lib/Button.svelte';
+
+  
+  onMount(async () => {
+    const fs = new FileSystem('fs').promises;
+    const dir = '/1';
+    // await fs.mkdir(dir);
+    // await fs.mkdir(`${dir}/.git/config`);
+
+    const url = 'https://github.com/craigfay/string_compressor';
+    const corsProxy = 'https://cors.isomorphic-git.org';
+    await git.clone({ fs, http, dir, url }).then(console.log)
+
+    let contents = await fs.readdir(dir);
+    console.log(contents)
+  })
+
+
+
+</script>
+
+
