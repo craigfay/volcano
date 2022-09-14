f9fbc15e679990870f087f5c5909010c0adb0ab6
craigfay <craigfay1@gmail.com>
2022-09-14 00:40:41 -0500

    Adjust menu styles

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -3,6 +3,7 @@
 	import HamburgerIcon from './HamburgerIcon.svelte';
 	import ChevronIcon from './ChevronIcon.svelte';
 	import Link from '$lib/Link.svelte';
+	import { color } from '$lib/style.js'
 
 	export let isOpen = false;
 	export let closeBehavior = () => {}; 
@@ -26,14 +27,14 @@
 >
 	<!-- Menu Close Button -->
 	<button aria-label="close menu" on:click={closeBehavior} class="block ml-auto mb-4">
-		<HamburgerIcon isOpen={true} color="#444" class="float-right w-8 h-8" />
+		<HamburgerIcon isOpen={true} color={color[900]} class="float-right w-8 h-8" />
 	</button>
 
 	<!-- Menu Items -->
 	{#each menuItems as {name, href, clearHistory }}
 		<Link {href} {clearHistory} onClick={closeBehavior} class="flex justify-between items-center">
-			<span class="my-3 text-xl font-bold block">{name}</span>
-			<ChevronIcon color="#666" class="w-5 h-5" />
+			<span class="my-3 text-indigo-900 text-xl font-bold block">{name}</span>
+			<ChevronIcon color={color[900]} class="w-5 h-5" />
 		</Link>
 	{/each}
 </div>
