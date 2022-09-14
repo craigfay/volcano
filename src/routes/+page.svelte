c914f68090eb84f9ad3e63cdd9afe6073525ce7d
craigfay <craigfay1@gmail.com>
2022-09-14 00:52:03 -0500

    Format notebook list items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,22 +21,22 @@
 </script>
 
 
-<h1 class="text-4xl font-bold text-indigo-900 m-6">Notebooks 📖</h1>
+<h1 class="text-4xl font-bold text-indigo-900 m-4 my-6">Notebooks 📖</h1>
 
 {#if isLoaded && !notebooks.length}
-  <p class="text-lg text-indigo-800 m-4 p-4 text-center bg-indigo-100 rounded-lg">
+  <p class="text-lg text-indigo-800 m-4 mb-8 p-4 text-center bg-indigo-100 rounded-lg">
     You don't have any notebooks yet...<br />
     <Link href="/create-new" class="underline">Create a new one!</Link> ✏
   </p>
 {:else}
-  <p class="text-lg text-indigo-800 m-4 p-4 text-center bg-indigo-100 rounded-lg">
+  <p class="text-lg text-indigo-800 m-4 mb-6 p-4 text-center bg-indigo-100 rounded-lg">
     <Link href="/create-new" class="font-bold">New Notebook</Link> ✏
   </p>
 {/if}
 
 {#each notebooks as notebook}
   <div class="p-4 mb-4 mx-4 rounded-lg border-2 border-indigo-200">
-    <div class="text-xl font-bold text-indigo-800">{notebook.name}</div>
-    <div class="text-indigo-700">{notebook.description}</div>
+    <div class="text-xl font-bold text-indigo-800 truncate whitespace-pre-wrap">{notebook.name}</div>
+    <div class="text-indigo-700 truncate whitespace-pre-wrap">{notebook.description}</div>
   </div>
 {/each}
